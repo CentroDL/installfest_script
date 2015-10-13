24ba6fdf8e6d3ab6652f770c18d69d43488b38a1
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2015-10-13 17:11:01 -0400

    Change Text Colors.
    Replaces pause_awhile and warn text colors to black so it's easier
    to read.

--- scripts/utils/log_screen.sh
+++ scripts/utils/log_screen.sh
@@ -11,11 +11,11 @@ function inform () {
 
 function warn () {
   if [[ $2 ]]; then echo ""; fi
-  echo -e "${BG_RED}${WHITE}${BOLD}>>>>  $1 ${RESET}"
+  echo -e "${BG_RED}${BLACK}${BOLD}>>>>  $1 ${RESET}"
 }
 
 function pause_awhile () {
   if [[ $2 ]]; then echo ""; fi
-  echo -e "${BG_YELLOW}${WHITE}${BOLD}>>>>  $1 ${RESET}"
-  read -p "${BG_YELLOW}${WHITE}${BOLD}Press <Enter> to continue.${RESET}"
+  echo -e "${BG_YELLOW}${BLACK}${BOLD}>>>>  $1 ${RESET}"
+  read -p "${BG_YELLOW}${BLACK}${BOLD}Press <Enter> to continue.${RESET}"
 }
